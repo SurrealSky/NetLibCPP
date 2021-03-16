51127e704c86556064ba798c292d4cc2b22f9fe8
SurrealSky <40445799+SurrealSky@users.noreply.github.com>
2021-03-16 17:25:59 +0800

    更新

--- NetLibCpp/HTTPDemo/HTTPDemo.cpp
+++ NetLibCpp/HTTPDemo/HTTPDemo.cpp
@@ -66,14 +66,66 @@ void test_google()
 	http.UnInitialize();
 }
 
-void test_yingyongbao()
+bool test_yingyongbao(CNetHttp& http, std::vector<std::string>& headers, std::string packetname, std::string& appname)
 {
-	CNetHttp http;
-	http.CoInitialize();
 	std::string host = "https://a.app.qq.com";
 	std::string url = "/o/simple.jsp";
 	unsigned int port = 443;
-	std::string querystring = "?pkgname=com.tencent.mobileqq";
+	
+	ByteBuffer response;
+	std::string querystring = "?pkgname=" + packetname;
+	http.perform_get(host, url, port, querystring, headers, &response);
+	//查找结果
+	std::string value;
+	value.append((char*)response.contents(), response.size());
+	const char* notfindtag = "http://a.app.qq.com/error_pages/noApp.jsp";
+	const char* findtag = "itemprop=\"name\"";
+	int pos = value.find(findtag);
+	if (pos != -1)
+	{
+		//找到了
+		appname = value.substr(pos + 25, 0x50);
+		pos = appname.find_first_of('"');
+		appname = appname.substr(0, pos);
+		return true;
+	}
+	appname = "";
+	return false;
+}
+
+bool test_xiaomi(CNetHttp& http,std::vector<std::string> &headers,std::string packetname,std::string &appname)
+{
+	std::string host = "https://app.mi.com";
+	std::string url = "/details";
+	unsigned int port = 443;
+
+	ByteBuffer response;
+	std::string querystring = "?id=" + packetname;
+	http.perform_get(host, url, port, querystring, headers, &response);
+	//查找结果
+	std::string value;
+	value.append((char*)response.contents(), response.size());
+	const char* notfindtag = "The URL has moved <a href=";
+	const char* findtag = "class=\"intro-titles\"";
+	int pos = value.find(findtag);
+	if (pos != -1)
+	{
+		//找到了
+		appname = value.substr(pos + 50, 0x50);
+		pos = appname.find_first_of('<');
+		appname = appname.substr(0, pos);
+		return true;
+	}
+	appname = "";
+	return false;
+}
+
+
+
+void common_get()
+{
+	CNetHttp http;
+	http.CoInitialize();
 	std::vector<std::string> headers;
 	headers.push_back("Accept-Encoding: gzip, deflate");
 	headers.push_back("Accept: image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/x-shockwave-flash, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, application/x-ms-application, application/x-ms-xbap, application/vnd.ms-xpsdocument, application/xaml+xml, */*");
@@ -82,46 +134,37 @@ void test_yingyongbao()
 	headers.push_back("Connection: close");
 
 	//输入文件
-	std::string file = "C:\\Users\\taiji\\Desktop\\1.txt";
+	std::string file = "C:\\Users\\taiji\\Desktop\\query\\in\\16.txt";
 	std::ifstream in(file);
 	//输出文件
-	std::fstream fusername("C:\\Users\\taiji\\Desktop\\out.txt", std::ios::out | std::ios::app);
+	std::fstream fusername1("C:\\Users\\taiji\\Desktop\\query\\out\\16.txt", std::ios::out | std::ios::app);
+	std::fstream fusername2("C:\\Users\\taiji\\Desktop\\query\\out\\16unknow.txt", std::ios::out | std::ios::app);
 	std::string packetname;
+	std::string appname = "";
 	while (getline(in, packetname))
 	{
-		ByteBuffer response;
-		querystring = "?pkgname=" + packetname;
-		http.perform_get(host, url, port, querystring, headers, &response);
-		//查找结果
-		std::string value;
-		value.append((char*)response.contents(), response.size());
-		const char* notfindtag = "http://a.app.qq.com/error_pages/noApp.jsp";
-		const char* findtag = "itemprop=\"name\"";
-		int pos = value.find(findtag);
-		if (pos != -1)
+		if (test_xiaomi(http, headers, packetname, appname))
 		{
-			//找到了
-			std::string appname = value.substr(pos + 25, 0x50);
-			pos = appname.find_first_of('"');
-			appname = appname.substr(0, pos);
-			fusername << packetname.c_str() << ":" << appname << std::endl;
+			//先查询小米
+			fusername1 << packetname.c_str() << ":" << appname.c_str() << std::endl;
 		}
 		else
 		{
-			pos = value.find(notfindtag);
-			if (pos != -1)
+			//再查询腾讯应用宝
+			if (test_yingyongbao(http, headers, packetname, appname))
 			{
-				//没有找到
-				fusername << packetname.c_str() << ":" << std::endl;
-			}
+				fusername1 << packetname.c_str() << ":" << appname.c_str() << std::endl;
+			}else
+				fusername2 << packetname.c_str()  << std::endl;
 		}
 	}
-	fusername.close();
+	fusername1.close();
+	fusername2.close();
 	http.UnInitialize();
 }
 
 int main()
 {
 	// TODO: 在此添加控件通知处理程序代码
-	test_yingyongbao();
+	common_get();
 }
